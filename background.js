ef0f8ec8280f28fd40f39bf58bd1a09de8e173c4
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-09 00:44:00 +0100

    started over :/

--- background.js
+++
@@ -1,9 +0,0 @@
-// listen for messages from content.js
-chrome.runtime.onMessage.addListener((message) => {
-    if(message.action === 'screenshot') {
-        //capture a screenshot
-        const screenshotDataUrl = message.dataUrl;
-        console.log('screenshot url:', screenshotDataUrl);
-    
-    }
-})

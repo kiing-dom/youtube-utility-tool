47ab43863afb041e38dbcfeace5b05387438538e
Dominion Gbadamosi <worshipgbadamosi@live.com>
2024-06-28 14:28:59 +0100

    feat: removed popup files no need to have them | chore: added new donation option

--- background.js
+++ background.js
@@ -7,3 +7,10 @@ chrome.commands.onCommand.addListener(command => {
         });
     }
 });
+
+chrome.action.onClicked.addListener((tab) => {
+    chrome.tabs.create({ url: chrome.runtime.getURL("options.html") });
+  });
+  
+
+
